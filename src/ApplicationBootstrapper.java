b6091b428e898fbaa3e6207d561b855f4cf0cf74
Wing <wing@busschots.ie>
2023-10-20 17:38:00 +0100

    swapping back

--- src/ApplicationBootstrapper.java
+++ src/ApplicationBootstrapper.java
@@ -3,20 +3,26 @@ import java.util.ArrayList;
 
 import cs171.project2024.kearns.eamonn.*;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * A class to bootstrap the application, extends PApplet to handle all graphics stuff
  * because I doubt whatever native libraries exist are as good
  */
-public class ApplicationBootstrapper 
+public class ApplicationBootstrapper extends Application
 {
 	private static Game game;
+
+	public void start(Stage stage)
+	{
+		
+	}
 	
 	public static void main(String[] args)
 	{
-		game = new Game(8);
+		game = new Game();
 		
-		System.out.println(game);
 	}
 
 }
