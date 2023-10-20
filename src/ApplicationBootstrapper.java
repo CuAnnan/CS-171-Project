6a96f5c12c7e4cac62af4f18d5a7e598397e3b93
Wing <wing@busschots.ie>
2023-10-20 17:46:13 +0100

    Trying to clean

--- src/ApplicationBootstrapper.java
+++ src/ApplicationBootstrapper.java
@@ -3,26 +3,19 @@ import java.util.ArrayList;
 
 import cs171.project2024.kearns.eamonn.*;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
 /**
  * A class to bootstrap the application, extends PApplet to handle all graphics stuff
  * because I doubt whatever native libraries exist are as good
  */
-public class ApplicationBootstrapper extends Application
+public class ApplicationBootstrapper
 {
 	private static Game game;
 
-	public void start(Stage stage)
-	{
-		
-	}
 	
 	public static void main(String[] args)
 	{
 		game = new Game();
-		
+		System.out.println(game);
 	}
 
 }
